cb49f302e7962c9cc4c64f65101817bd6f19426b
Jean-Simon Robert Ouimet <jsrobertouimet@gmail.com>
2018-10-08 20:03:35 -0400

    Removed client from gitignore

---
+++ client/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <PostsComponent />
+  </div>
+</template>
+
+<script>
+import PostsComponent from './components/PostsComponent.vue'
+
+export default {
+  name: 'app',
+  components: {
+    PostsComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
